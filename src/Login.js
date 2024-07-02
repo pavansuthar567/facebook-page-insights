71b71534077f525c0ff14647472cfb4df853e2ed
Pavan Suthar <53314417+pavansuthar567@users.noreply.github.com>
2024-07-02 09:36:13 +0530

    Facebook SDK config Changes | Design Changes |
    Facebook SDK config Changes | Design Changes | Added Filters By Dates

--- src/Login.js
+++ src/Login.js
@@ -12,16 +12,33 @@ import {
   Card,
   CardContent,
   Grid,
+  TextField,
 } from "@mui/material";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+
+const periodOptions = [
+  { label: "Day", value: "day" },
+  { label: "Week", value: "week" },
+  { label: "Days 28", value: "days_28" },
+  { label: "Month", value: "month" },
+  { label: "Lifetime", value: "lifetime" },
+  { label: "Total Over Range", value: "total_over_range" },
+];
 
 const Login = () => {
   const [user, setUser] = useState(null);
   const [pages, setPages] = useState([]);
   const [insights, setInsights] = useState({});
+  const [pageId, setPageId] = useState("");
+  const [period, setPeriod] = useState("days_28");
+  const [since, setSince] = useState(null);
+  const [until, setUntil] = useState(null);
+  const [error, setError] = useState("");
 
   const fetchUserProfile = useCallback(() => {
     window.FB.api("/me", { fields: "name,email,picture" }, function (response) {
-      console.log("response", response);
+      console.log("response user details", response);
       setUser(response);
       fetchUserPages();
     });
@@ -37,45 +54,25 @@ const Login = () => {
   );
 
   useEffect(() => {
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: process.env.REACT_APP_FACEBOOK_APP_ID,
-        cookie: true,
-        xfbml: true,
-        version: "v20.0",
-      });
-
-      window.FB.AppEvents.logPageView();
-
-      // Check if user is already logged in
-      window.FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-      });
-    };
-
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
+    // Check if user is already logged in
+    window.FB.getLoginStatus(function (response) {
+      console.log("response login status", response);
+      statusChangeCallback(response);
+    });
   }, [statusChangeCallback]);
 
   const fetchUserPages = () => {
     window.FB.api("/me/accounts", function (response) {
-      console.log("response.data", response.data);
+      console.log("Pages", response.data);
       setPages(response.data);
+      setPageId(response?.data?.[0]?.id);
     });
   };
 
   const handleLogin = () => {
     window.FB.login((response) => {
-      if (response.authResponse) {
+      console.log("response login", response);
+      if (response?.authResponse) {
         fetchUserProfile();
       }
     });
@@ -83,31 +80,56 @@ const Login = () => {
 
   const handlePageChange = (e) => {
     const pageId = e.target.value;
-    if (pageId) fetchPageInsights(pageId);
+    setPageId(pageId);
   };
 
-  const fetchPageInsights = async (pageId) => {
+  const handlePeriodChange = (e) => {
+    const period = e.target.value;
+    if (period) setPeriod(period);
+  };
+
+  const fetchPageInsights = useCallback(() => {
+    if (!pageId) return;
+    setError("");
+
     const page = pages?.find((x) => x?.id === pageId);
-    window.FB.api(
-      // `/${pageId}/insights/page_impressions_unique`,
-      `/${pageId}/insights?metric=page_impressions_unique,page_fans,page_video_views,post_reactions_like_total&period=total_over_range&access_token=${page?.access_token}`,
-      //
-      function (response) {
-        console.log("response", response);
+
+    const sinceDate = since ? since.toISOString().split("T")[0] : "";
+    const untilDate = until ? until.toISOString().split("T")[0] : "";
+    const metrics = `page_impressions,page_actions_post_reactions_total,page_fan_adds,page_post_engagements,page_fans,page_video_views,post_reactions_like_total`;
+
+    const url = `/${pageId}/insights?metric=${metrics}&period=${period}&since=${sinceDate}&until=${untilDate}&access_token=${page?.access_token}`;
+
+    window.FB.api(url, function (response) {
+      console.log("response insights", response);
+      if (response?.error) {
+        setError(response?.error?.error_user_msg);
+        setInsights({});
+      } else {
         const insightsData = response?.data?.reduce((acc, item) => {
           acc[item?.name] = item?.values?.[0]?.value;
           return acc;
         }, {});
+        console.log("insightsData", insightsData);
         setInsights(insightsData);
       }
-    );
-  };
+    });
+  }, [pageId, pages, period, since, until]);
+
+  const onLogout = useCallback(() => {
+    window.FB.logout(function (response) {
+      console.log("Person is now logged out!!");
+      setUser(null);
+    });
+  }, []);
 
   return (
     <Container>
+      <div id="facebook-jssdk"></div>
       {!user ? (
         <Box
           display="flex"
+          flexDirection={"column"}
           justifyContent="center"
           alignItems="center"
           minHeight="100vh"
@@ -120,36 +142,90 @@ const Login = () => {
         <Box mt={5}>
           <Card>
             <CardContent>
-              <Grid container spacing={2} alignItems="center">
-                <Grid item>
-                  <Avatar
-                    src={user.picture.data.url}
-                    alt="Profile"
-                    sx={{ width: 56, height: 56 }}
-                  />
+              <Grid
+                container
+                spacing={2}
+                alignItems="center"
+                flexDirection={"row"}
+                justifyContent="space-between"
+              >
+                <Grid item display={"flex"} alignItems={"center"}>
+                  <Grid item>
+                    <Avatar
+                      src={user?.picture?.data?.url}
+                      alt="Profile"
+                      sx={{ width: 56, height: 56 }}
+                    />
+                  </Grid>
+                  <Grid item ml={2}>
+                    <Typography variant="h4">Welcome, {user?.name}</Typography>
+                  </Grid>
                 </Grid>
                 <Grid item>
-                  <Typography variant="h4">Welcome, {user.name}</Typography>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={onLogout}
+                  >
+                    Logout
+                  </Button>
                 </Grid>
               </Grid>
-              <Box mt={3}>
-                <FormControl fullWidth>
-                  <InputLabel>
-                    Select a Page
-                  </InputLabel>
-                  <Select onChange={handlePageChange} defaultValue="">
+
+              <Box display="flex" justifyContent={"space-between"} mt={3}>
+                <FormControl style={{ marginRight: "20px", width: "25%" }}>
+                  <InputLabel>Select a Page</InputLabel>
+                  <Select onChange={handlePageChange} value={pageId}>
                     <MenuItem value="">
                       <em>Select a Page</em>
                     </MenuItem>
-                    {pages.map((page) => (
+                    {pages?.map((page) => (
                       <MenuItem key={page.id} value={page.id}>
                         {page.name}
                       </MenuItem>
                     ))}
                   </Select>
                 </FormControl>
+                <FormControl style={{ marginRight: "20px", width: "25%" }}>
+                  <InputLabel>Select a Period</InputLabel>
+                  <Select onChange={handlePeriodChange} value={period}>
+                    {periodOptions?.map((page) => (
+                      <MenuItem key={page.value} value={page.value}>
+                        {page.label}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <Box display="flex" mr={3}>
+                    <DatePicker
+                      label="Since"
+                      value={since}
+                      onChange={(newValue) => setSince(newValue)}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </Box>
+                  <Box display="flex">
+                    <DatePicker
+                      label="Until"
+                      value={until}
+                      onChange={(newValue) => setUntil(newValue)}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </Box>
+                </LocalizationProvider>
               </Box>
-              {Object.keys(insights).length > 0 && (
+              <Button
+                style={{ marginTop: "20px" }}
+                onClick={fetchPageInsights}
+                variant="contained"
+                color="primary"
+              >
+                Get Insights
+              </Button>
+              {error && <p>{error}</p>}
+
+              {Object.keys(insights || {}).length > 0 && (
                 <Box mt={5}>
                   <Typography variant="h5">Page Insights</Typography>
                   <Grid container spacing={2} mt={2}>
@@ -157,7 +233,7 @@ const Login = () => {
                       <Card>
                         <CardContent>
                           <Typography variant="h6">Total Followers</Typography>
-                          <Typography>{insights.page_fans}</Typography>
+                          <Typography>{insights?.page_fans || 0}</Typography>
                         </CardContent>
                       </Card>
                     </Grid>
@@ -165,7 +241,9 @@ const Login = () => {
                       <Card>
                         <CardContent>
                           <Typography variant="h6">Total Engagement</Typography>
-                          <Typography>{insights.page_engaged_users}</Typography>
+                          <Typography>
+                            {insights?.page_post_engagements || 0}
+                          </Typography>
                         </CardContent>
                       </Card>
                     </Grid>
@@ -175,7 +253,9 @@ const Login = () => {
                           <Typography variant="h6">
                             Total Impressions
                           </Typography>
-                          <Typography>{insights.page_impressions}</Typography>
+                          <Typography>
+                            {insights?.page_impressions || 0}
+                          </Typography>
                         </CardContent>
                       </Card>
                     </Grid>
@@ -183,7 +263,20 @@ const Login = () => {
                       <Card>
                         <CardContent>
                           <Typography variant="h6">Total Reactions</Typography>
-                          <Typography>{insights.page_reactions}</Typography>
+                          <Typography>
+                            {insights?.page_actions_post_reactions_total
+                              ?.like || 0}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                      <Card>
+                        <CardContent>
+                          <Typography variant="h6">Page Video Views</Typography>
+                          <Typography>
+                            {insights?.page_video_views || 0}
+                          </Typography>
                         </CardContent>
                       </Card>
                     </Grid>
